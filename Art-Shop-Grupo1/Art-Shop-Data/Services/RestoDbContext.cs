d85957ce132874cca5b75a2f60fae704f0ee5a5e
JdetittoUAI <julianeloy.detitto@gmail.com>
2020-06-28 20:46:58 -0300

    Intefaces base

---
+++ Art-Shop-Grupo1/Art-Shop-Data/Services/RestoDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Art_Shop_Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Art_Shop_Data.Services
+{
+    public partial class RestoDbContext : DbContext
+    {
+        public RestoDbContext() : base("name=DefaultConnection")
+        {
+            Database.SetInitializer<RestoDbContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+        /// DbSet Artist se utiliza para representar una tabla.
+        public virtual DbSet<Artist> Artist { get; set; }
+
+        /// DbSet Product se utiliza para representar una tabla.
+        public virtual DbSet<Product> Product { get; set; }
+        /// DbSet Product se utiliza para representar una tabla.
+        public virtual DbSet<Error> Error { get; set; }
+
+    }
+}
